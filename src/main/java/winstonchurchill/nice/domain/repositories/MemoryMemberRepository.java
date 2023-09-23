a42f7604721471e65a6e95b1e176896d3cd9a3f9
maaaaaaaaad <integral5137@gmail.com>
2023-09-23 16:58:37 +0900

    e

---
+++ src/main/java/winstonchurchill/nice/domain/repositories/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package winstonchurchill.nice.domain.repositories;
+
+import winstonchurchill.nice.domain.entity.Member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements IMemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+}
