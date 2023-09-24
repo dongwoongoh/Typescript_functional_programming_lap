2c4ef83d1389181e66fdcb9bde0fe5eedf603760
maaaaaaaaad <integral5137@gmail.com>
2023-09-24 19:01:06 +0900

    e

--- src/main/java/winstonchurchill/nice/domain/member/services/IDiscount.java
+++
@@ -1,7 +0,0 @@
-package winstonchurchill.nice.domain.member.services;
-
-import winstonchurchill.nice.domain.member.entity.Member;
-
-public interface IDiscount {
-    int discount(Member member);
-}
