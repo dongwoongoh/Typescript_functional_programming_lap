e7c35b260a7396d3c901e1a1eb78be9d299a5e68
maaaaaaaaad <integral5137@gmail.com>
2023-09-25 18:50:47 +0900

    e

---
+++ src/test/java/winstonchurchill/nice/order/services/OrderServiceTest.java
@@ -0,0 +1,33 @@
+package winstonchurchill.nice.order.services;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import winstonchurchill.nice.domain.member.entity.Member;
+import winstonchurchill.nice.domain.member.repositores.IMemberRepository;
+import winstonchurchill.nice.domain.member.repositores.MemoryMemberRepository;
+import winstonchurchill.nice.domain.member.types.TGrade;
+import winstonchurchill.nice.domain.order.entity.Order;
+import winstonchurchill.nice.domain.order.repositories.IOrderRepository;
+import winstonchurchill.nice.domain.order.repositories.MemoryOrderRepository;
+import winstonchurchill.nice.domain.order.services.IOrderService;
+import winstonchurchill.nice.domain.order.services.OrderService;
+
+public class OrderServiceTest {
+    private final IOrderService orderService = new OrderService();
+    private final IMemberRepository memberRepository = new MemoryMemberRepository();
+    private final IOrderRepository orderRepository = new MemoryOrderRepository();
+
+    final int itemPrice = 11000;
+    final String itemName = "mad-item";
+
+    @Test
+    void save() {
+        final Member mad = new Member(1L, "mad", TGrade.VIP);
+        this.memberRepository.save(mad);
+        final Member member = this.memberRepository.findById(mad.getId());
+        final Order order = this.orderService.createOrder(1L, member.getId(), itemName, itemPrice);
+        this.orderRepository.save(order);
+        final Order orderRecord = this.orderRepository.findById(order.getId());
+        Assertions.assertThat(order.getTotalPrice()).isEqualTo(orderRecord.getTotalPrice());
+    }
+}
