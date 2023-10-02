4528b07de2c41533e72e5637247663311461c640
maaaaaaaaad <integral5137@gmail.com>
2023-10-02 20:43:27 +0900

    e

--- src/main/java/winstonchurchill/nice/configs/AppRunConfig.java
+++ src/main/java/winstonchurchill/nice/configs/AppRunConfig.java
@@ -1,12 +1,21 @@
 package winstonchurchill.nice.configs;
 
+import winstonchurchill.nice.applications.usecases.discount.RateDiscountPolicy;
 import winstonchurchill.nice.domain.member.repositores.MemoryMemberRepository;
 import winstonchurchill.nice.domain.member.services.IMemberService;
 import winstonchurchill.nice.domain.member.services.MemberService;
+import winstonchurchill.nice.domain.order.repositories.IOrderRepository;
+import winstonchurchill.nice.domain.order.repositories.MemoryOrderRepository;
+import winstonchurchill.nice.domain.order.services.IOrderService;
+import winstonchurchill.nice.domain.order.services.OrderService;
 
 public class AppRunConfig {
 
     public IMemberService memberService() {
         return new MemberService(new MemoryMemberRepository());
+    }
+
+    public IOrderService orderService() {
+        return new OrderService(new MemoryOrderRepository(), new RateDiscountPolicy(), new MemoryMemberRepository());
     }
 }
--- src/test/java/winstonchurchill/nice/order/services/OrderServiceTest.java
+++ src/test/java/winstonchurchill/nice/order/services/OrderServiceTest.java
@@ -1,48 +1,19 @@
 package winstonchurchill.nice.order.services;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import winstonchurchill.nice.domain.member.entity.Member;
-import winstonchurchill.nice.domain.member.repositores.IMemberRepository;
-import winstonchurchill.nice.domain.member.repositores.MemoryMemberRepository;
-import winstonchurchill.nice.domain.member.types.MemberGrade;
-import winstonchurchill.nice.domain.order.entity.Order;
-import winstonchurchill.nice.domain.order.repositories.IOrderRepository;
-import winstonchurchill.nice.domain.order.repositories.MemoryOrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import winstonchurchill.nice.configs.AppRunConfig;
 import winstonchurchill.nice.domain.order.services.IOrderService;
-import winstonchurchill.nice.domain.order.services.OrderService;
 
 public class OrderServiceTest {
-    private final IOrderService orderService = new OrderService();
-    private final IMemberRepository memberRepository = new MemoryMemberRepository();
-    private final IOrderRepository orderRepository = new MemoryOrderRepository();
+    private IOrderService orderService;
+
+    @BeforeEach
+    public void before() {
+        final AppRunConfig appRunConfig = new AppRunConfig();
+        this.orderService = appRunConfig.orderService();
+    }
 
     final int itemPrice = 11000;
     final String itemName = "mad-item";
 
-    @Test
-    void save() {
-        final Member mad = new Member(1L, "mad", MemberGrade.VIP);
-        this.memberRepository.save(mad);
-        final Member member = this.memberRepository.findById(mad.getId());
-        final Order order = this.orderService.createOrder(1L, member.getId(), itemName, itemPrice);
-        final Order orderRecord = this.orderRepository.findById(order.getId());
-        Assertions.assertThat(order.getTotalPrice()).isEqualTo(orderRecord.getTotalPrice());
-    }
-
-    @Test
-    void find() {
-        final Member mad = new Member(2L, "mad1L", MemberGrade.VIP);
-        this.memberRepository.save(mad);
-        final Member member = this.memberRepository.findById(mad.getId());
-        final Order order = this.orderService.createOrder(2L, member.getId(), itemName, itemPrice);
-        final Order orderRecord = this.orderService.findOrder(order.getId());
-        Assertions.assertThat(orderRecord.getId()).isEqualTo(order.getId());
-        Assertions.assertThat(orderRecord.getMemberId()).isEqualTo(order.getMemberId());
-        Assertions.assertThatThrownBy(() -> {
-                    this.orderService.findOrder(33L);
-                })
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("orderId doesn't exist");
-    }
 }
