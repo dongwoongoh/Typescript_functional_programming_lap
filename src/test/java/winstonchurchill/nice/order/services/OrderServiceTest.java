3685d3f677ca6d06e6a3e4156b5755cd189abdc7
maaaaaaaaad <integral5137@gmail.com>
2023-10-02 20:53:19 +0900

    e

--- src/test/java/winstonchurchill/nice/order/services/OrderServiceTest.java
+++ src/test/java/winstonchurchill/nice/order/services/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package winstonchurchill.nice.order.services;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,7 +31,18 @@ public class OrderServiceTest {
         final Member mad = new Member(1L, "mad", MemberGrade.VIP);
         this.memberService.join(mad);
         final Order order = this.orderService.createOrder(1L, mad.getId(), this.itemName, this.itemPrice);
-        System.out.println(order.toString());
+        Assertions.assertThat(order.getId()).isEqualTo(1L);
+        Assertions.assertThat(order.getMemberId()).isEqualTo(1L);
     }
 
+    @Test
+    @DisplayName("should find item by id")
+    void find() {
+        final Member mad = new Member(99L, "mad", MemberGrade.VIP);
+        this.memberService.join(mad);
+        this.orderService.createOrder(99L, mad.getId(), this.itemName, this.itemPrice);
+        final Order order = this.orderService.findOrder(99L);
+        Assertions.assertThat(order.getId()).isEqualTo(99L);
+        Assertions.assertThat(order.getMemberId()).isEqualTo(99L);
+    }
 }
