91ccf4c276db528db4e018e110b15e09cb34d870
maaaaaaaaad <integral5137@gmail.com>
2023-10-05 09:35:15 +0900

    e

---
+++ src/main/java/winstonchurchill/nice/domain/point/entity/Point.java
@@ -0,0 +1,22 @@
+package winstonchurchill.nice.domain.point.entity;
+
+public class Point {
+    private Long id;
+    private Long MemberId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return MemberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        MemberId = memberId;
+    }
+}
