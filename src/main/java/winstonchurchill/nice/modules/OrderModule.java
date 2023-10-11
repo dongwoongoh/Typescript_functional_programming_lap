8cfe7a910063b55169ac0f1ede8d52377e2b7439
maaaaaaaaad <integral5137@gmail.com>
2023-10-12 05:07:49 +0900

    e

--- src/main/java/winstonchurchill/nice/modules/OrderModule.java
+++ src/main/java/winstonchurchill/nice/modules/OrderModule.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import winstonchurchill.nice.configs.AppRunConfig;
 import winstonchurchill.nice.domain.member.entity.Member;
+import winstonchurchill.nice.domain.member.services.IMemberService;
+import winstonchurchill.nice.domain.member.services.MemberService;
 import winstonchurchill.nice.domain.member.types.MemberGrade;
 import winstonchurchill.nice.domain.order.entity.Order;
 import winstonchurchill.nice.domain.order.services.IOrderService;
@@ -12,9 +14,12 @@ import winstonchurchill.nice.domain.order.services.OrderService;
 public class OrderModule {
     public static void main(String[] args) {
         final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppRunConfig.class);
+        final IMemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         final IOrderService orderService = applicationContext.getBean("orderService", OrderService.class);
         final Member member = new Member(1L, "madmad", MemberGrade.VIP);
-        final Order order = orderService.createOrder(2L, member.getId(), "mad-item", 1000);
+        memberService.join(member);
+        final Member mad = memberService.findMember(member.getId());
+        final Order order = orderService.createOrder(2L, mad.getId(), "mad-item", 1000);
         final Order result = orderService.findOrder(order.getId());
         System.out.println(result.toString());
     }
