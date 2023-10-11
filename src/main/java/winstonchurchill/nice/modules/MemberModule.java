cbe09d21bf8c66b43274ceb038b040cd10cf2ea9
maaaaaaaaad <integral5137@gmail.com>
2023-10-11 12:12:34 +0900

    e

--- src/main/java/winstonchurchill/nice/modules/MemberModule.java
+++ src/main/java/winstonchurchill/nice/modules/MemberModule.java
@@ -3,13 +3,18 @@ package winstonchurchill.nice.modules;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import winstonchurchill.nice.configs.AppRunConfig;
+import winstonchurchill.nice.domain.member.entity.Member;
 import winstonchurchill.nice.domain.member.services.IMemberService;
 import winstonchurchill.nice.domain.member.services.MemberService;
+import winstonchurchill.nice.domain.member.types.MemberGrade;
 
 public class MemberModule {
     public static void main(String[] args) {
         final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppRunConfig.class);
         final IMemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-
+        final Member member = new Member(1L, "mad", MemberGrade.VIP);
+        memberService.join(member);
+        final Member result = memberService.findMember(member.getId());
+        System.out.println(result.toString());
     }
 }
