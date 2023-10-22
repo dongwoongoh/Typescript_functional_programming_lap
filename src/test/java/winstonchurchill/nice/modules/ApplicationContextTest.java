32597f1660d1ee1ed5d649dfb43200db187504b0
maaaaaaaaad <integral5137@gmail.com>
2023-10-22 17:03:38 +0900

    e

--- src/test/java/winstonchurchill/nice/modules/ApplicationContextTest.java
+++ src/test/java/winstonchurchill/nice/modules/ApplicationContextTest.java
@@ -2,21 +2,24 @@ package winstonchurchill.nice.modules;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import winstonchurchill.nice.configs.AppRunConfig;
 
 public class ApplicationContextTest {
 
-    final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppRunConfig.class);
+    final AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppRunConfig.class);
 
     @Test
     @DisplayName("should find all bean")
     void findAll() {
         final String[] getBeanDefinitionNames = applicationContext.getBeanDefinitionNames();
         for (String getBeanDefinitionName : getBeanDefinitionNames) {
-            final Object bean = applicationContext.getBean(getBeanDefinitionName);
-            System.out.println(bean);
+            final BeanDefinition beanDefinition = applicationContext.getBeanDefinition(getBeanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                final Object bean = applicationContext.getBean(getBeanDefinitionName);
+                System.out.println("name = " + getBeanDefinitionName + " bean = " + bean);
+            }
         }
     }
 
