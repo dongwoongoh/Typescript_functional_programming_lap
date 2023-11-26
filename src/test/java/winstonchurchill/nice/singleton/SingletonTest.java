501b7f74831168f6ddc2198c011e316165eed254
maaaaaaaaad <integral5137@gmail.com>
2023-11-27 08:52:26 +0900

    e

--- src/test/java/winstonchurchill/nice/singleton/SingletonTest.java
+++ src/test/java/winstonchurchill/nice/singleton/SingletonTest.java
@@ -3,6 +3,9 @@ package winstonchurchill.nice.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import winstonchurchill.nice.configs.AppRunConfig;
+import winstonchurchill.nice.domain.member.services.IMemberService;
 
 public class SingletonTest {
 
@@ -21,5 +24,14 @@ public class SingletonTest {
         SingletonTest test1 = new SingletonTest();
         SingletonTest test2 = new SingletonTest();
         Assertions.assertThat(test1).isNotSameAs(test2);
+    }
+
+    @Test
+    @DisplayName("should same this two service via app run config")
+    public void service() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppRunConfig.class);
+        final IMemberService memberService1 = applicationContext.getBean("memberService", IMemberService.class);
+        final IMemberService memberService2 = applicationContext.getBean("memberService", IMemberService.class);
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
